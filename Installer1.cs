8aecdc87b9c91694c4a566709515d4fddf2cfcd3
amartyamandal <amartya.mandal@gmail.com>
2020-06-02 13:13:04 -0400

    First commit

---
+++ Installer1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace BrowserAutomation01
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : System.Configuration.Install.Installer
+    {
+        
+        public Installer1()
+        {
+            InitializeComponent();
+        }
+
+       
+        // Override the 'Install' method.
+        public override void Install(IDictionary savedState)
+        {
+            base.Install(savedState);
+            
+
+
+            string UserProfilePath = this.Context.Parameters["USERPROFILEPATH"].ToString().Trim();
+            string TwilioMoNum = this.Context.Parameters["TWILIOMONUM"].ToString().Trim();
+            string ToMoNum = this.Context.Parameters["TOMONUM"].ToString().Trim();
+            string TwAccountSid = this.Context.Parameters["TWACCOUNTSID"].ToString().Trim();
+            string TwAuthToken = this.Context.Parameters["TWAUTHTOKEN"].ToString().Trim();
+           
+
+            if (TwilioMoNum.Length != 0 && ToMoNum.Length == 0)
+                throw new Exception("To activate SMS feature you need both sender's and receiver's cell num, sender's number should be a Twilo number1 ");
+            if (TwilioMoNum.Length == 0 && ToMoNum.Length != 0)
+                throw new Exception("To activate SMS feature you need both sender's and receiver's cell num, sender's number should be a Twilo number2 ");
+            if ((TwilioMoNum.Length != 0 && ToMoNum.Length != 0) && (TwAccountSid.Length == 0 && TwAuthToken.Length == 0))
+                throw new Exception("To activate SMS feature you need Twilo account3");
+            if ((TwilioMoNum.Length != 0 && ToMoNum.Length != 0) && (TwAccountSid.Length != 0 && TwAuthToken.Length == 0))
+                throw new Exception("To activate SMS feature you need Twilo account4");
+            if ((TwilioMoNum.Length != 0 && ToMoNum.Length != 0) && (TwAccountSid.Length == 0 && TwAuthToken.Length != 0))
+                throw new Exception("To activate SMS feature you need Twilo account5");
+            //if ((TwilioMoNum.Length == 0 && ToMoNum.Length == 0) && (TwAccountSid.Length == 0 && TwAuthToken.Length == 0))
+            //    MessageBox.Show("Application will work, but without Twillo account and mobile numbers SMS feature won't work!!");
+
+
+        }
+        // Override the 'Commit' method.
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+
+            AddConfigurationFileDetails();
+            int length = Context.Parameters["TargetDir"].ToString().Length - 1;
+            string targetDir = Context.Parameters["TargetDir"].ToString().Substring(0, length);
+            string xml = targetDir + @"jobtemplate\CheckOut.xml";
+            
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(xml);
+                FindAndSetNodeValue(doc.ChildNodes, targetDir);
+                doc.Save(xml);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        // Override the 'Rollback' method.
+        public override void Rollback(IDictionary savedState)
+        {
+            base.Rollback(savedState);
+        }
+
+
+        //private void showParameters()
+        //{
+        //    StringBuilder sb = new StringBuilder();
+        //    StringDictionary myStringDictionary = this.Context.Parameters;
+        //    if (this.Context.Parameters.Count > 0)
+        //    {
+        //        foreach (string myString in this.Context.Parameters.Keys)
+        //        {
+        //            sb.AppendFormat("String={0} Value= {1}\n", myString,
+        //            this.Context.Parameters[myString]);
+        //        }
+        //    }
+        //    MessageBox.Show(sb.ToString());
+        //}
+
+        public void AddConfigurationFileDetails()
+        {
+            string UserProfilePath = this.Context.Parameters["USERPROFILEPATH"].ToString().Trim();
+            string TwilioMoNum = this.Context.Parameters["TWILIOMONUM"].ToString().Trim();
+            string ToMoNum = this.Context.Parameters["TOMONUM"].ToString().Trim();
+            string TwAccountSid = this.Context.Parameters["TWACCOUNTSID"].ToString().Trim();
+            string TwAuthToken = this.Context.Parameters["TWAUTHTOKEN"].ToString().Trim();
+
+            if (UserProfilePath.Length == 0 && TwilioMoNum.Length == 0 && ToMoNum.Length == 0 && TwAccountSid.Length == 0 && TwAuthToken.Length == 0)
+            {
+                // do nothing
+            }
+            else
+            {
+                try
+                {
+                    // Get the path to the executable file that is being installed on the target computer  
+                    string assemblypath = Context.Parameters["assemblypath"];
+                    //MessageBox.Show("assemblypath="+assemblypath);
+                    string appConfigPath = assemblypath + ".config";
+
+                    // Write the path to the app.config file  
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(appConfigPath);
+
+                    XmlNode configuration = null;
+                    foreach (XmlNode node in doc.ChildNodes)
+                        if (node.Name == "configuration")
+                            configuration = node;
+
+                    if (configuration != null)
+                    {
+                        //MessageBox.Show("configuration != null");  
+                        // Get the ‘appSettings’ node  
+                        XmlNode settingNode = null;
+                        foreach (XmlNode node in configuration.ChildNodes)
+                        {
+                            if (node.Name == "appSettings")
+                                settingNode = node;
+                        }
+
+                        if (settingNode != null)
+                        {
+                            //MessageBox.Show("settingNode != null");  
+                            //Reassign values in the config file  
+                            foreach (XmlNode node in settingNode.ChildNodes)
+                            {
+                                //MessageBox.Show("node.Value = " + node.Value);  
+                                if (node.Attributes == null)
+                                    continue;
+                                XmlAttribute attribute = node.Attributes["value"];
+                                //MessageBox.Show("attribute != null ");  
+                                //MessageBox.Show("node.Attributes['value'] = " + node.Attributes["value"].Value);  
+                                if (node.Attributes["key"] != null)
+                                {
+                                    //MessageBox.Show("node.Attributes['key'] != null ");  
+                                    //MessageBox.Show("node.Attributes['key'] = " + node.Attributes["key"].Value);  
+                                    //switch (node.Attributes["key"].Value)
+                                    //{
+                                    //    case "TestParameter":
+                                    //        attribute.Value = TESTPARAMETER;
+                                    //        break;
+                                    //}
+
+
+                                    if (node.Attributes["key"].Value == "UserProfilePath" && UserProfilePath.Length!=0)
+                                    {
+                                        attribute.Value = UserProfilePath;
+                                    }
+                                    if (node.Attributes["key"].Value == "TwilioMoNum" && TwilioMoNum.Length != 0)
+                                    {
+                                        attribute.Value = TwilioMoNum;
+                                    }
+                                    if (node.Attributes["key"].Value == "ToMoNum" && ToMoNum.Length != 0)
+                                    {
+                                        attribute.Value = ToMoNum;
+                                    }
+                                    if (node.Attributes["key"].Value == "TwAccountSid" && TwAccountSid.Length != 0)
+                                    {
+                                        attribute.Value = TwAccountSid;
+                                    }
+                                    if (node.Attributes["key"].Value == "TwAuthToken" && TwAuthToken.Length != 0)
+                                    {
+                                        attribute.Value = TwAuthToken;
+                                    }
+                                }
+                            }
+                        }
+                        doc.Save(appConfigPath);
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+
+        public void FindAndSetNodeValue(XmlNodeList pnodes, string targetdir)
+        {
+
+            string jobUser = Environment.UserDomainName + "\\" + Environment.UserName;
+            foreach (XmlNode chnode in pnodes)
+            {
+                XmlNode settingNode = null;
+                string nodename = chnode.Name;
+                switch (nodename)
+                {
+                    case "Author":
+                        settingNode = chnode;
+                        settingNode.InnerText = jobUser;
+                        break;
+
+                    case "UserId":
+                        settingNode = chnode;
+                        settingNode.InnerText = jobUser;
+                        break;
+                    case "Command":
+                        settingNode = chnode;
+                        settingNode.InnerText = targetdir + "BrowserAutomation01.exe";
+                        break;
+                    case "WorkingDirectory":
+                        settingNode = chnode;
+                        settingNode.InnerText = targetdir;
+                        break;
+                }
+
+                FindAndSetNodeValue(chnode.ChildNodes, targetdir);
+
+            }
+
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,302 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.XPath;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.TwiML.Voice;
+using Thread = System.Threading.Thread;
+
+namespace BrowserAutomation01
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public static IDictionary<string, object> vars { get; private set; }
+
+        static Configuration config = ConfigurationManager.OpenExeConfiguration(System.Reflection.Assembly.GetEntryAssembly().Location);
+
+        //static string runJob = string.Empty;
+        static string pathToCurrentUserProfile = string.Empty;
+        static string goToUrl = string.Empty;
+        static string twilioMoNum = string.Empty;
+        static string toMoNum = string.Empty;
+
+        [Obsolete]
+        static void Main(string[] args)
+        {
+            AppSettingsSection appSettingsSection = (AppSettingsSection)config.GetSection("appSettings");
+            KeyValueConfigurationCollection settings = appSettingsSection.Settings;
+            if (settings.Count != 0)
+            {
+
+                string runJob = ReturnSettings(settings, MyAppSettings.RunJob);
+                pathToCurrentUserProfile = ReturnSettings(settings, MyAppSettings.UserProfilePath);
+                goToUrl = ReturnSettings(settings, MyAppSettings.GoToUrl);
+                twilioMoNum = ReturnSettings(settings, MyAppSettings.TwilioMoNum);
+                toMoNum = ReturnSettings(settings, MyAppSettings.ToMoNum);
+
+                if (runJob.ToLower() == "yes")
+                    Checkout(settings);
+                else if (runJob.ToLower() == "no")
+                    log.Info("Checkout presently stopped!!");
+                else if (runJob.ToLower().Trim().Length == 0)
+                {
+                    log.Error("No Run Job Direction");
+                }
+
+            }
+            else
+            {
+                log.Error("No App Setting Values Exists!!");
+            }
+
+
+
+
+        }
+        static string ReturnSettings(KeyValueConfigurationCollection settings, MyAppSettings settingKey)
+        {
+            if (settings[settingKey.Value] != null && settings[settingKey.Value].Value.Trim().Length != 0)
+                return settings[settingKey.Value].Value.Trim();
+            else
+                return string.Empty;
+        }
+        private static void Checkout(KeyValueConfigurationCollection settings)
+        {
+            log.Info("Checkout started!!");
+            //Get value from AppSettings
+            
+            
+            string pathToCurrentUserProfilesDirectory = Environment.ExpandEnvironmentVariables("%APPDATA%") + @"\Mozilla\Firefox\Profiles";
+            string[] pathsToProfiles = Directory.GetDirectories(pathToCurrentUserProfilesDirectory);
+
+            string pathtoDefaultProfile = pathsToProfiles[0];
+            if (pathToCurrentUserProfile != string.Empty)
+                pathtoDefaultProfile = pathToCurrentUserProfile;
+
+
+
+            log.Info("Profile Path=" + pathtoDefaultProfile);
+            
+
+            if (pathsToProfiles.Length != 0)
+            {
+
+                FirefoxProfile profile = new FirefoxProfile(pathtoDefaultProfile);
+                profile.SetPreference("browser.tabs.loadInBackground", true);
+
+                FirefoxOptions options = new FirefoxOptions();
+                options.Profile = profile;
+                var driverService = FirefoxDriverService.CreateDefaultService(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+                FirefoxDriver driver = new FirefoxDriver(driverService, options);
+                WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
+                IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+                vars = new Dictionary<string, object>();
+                try
+                {
+
+                    //Get value from AppSettings
+                    if (goToUrl == string.Empty)
+                        driver.Navigate().GoToUrl("https://www.bigbasket.com/basket/");
+                    else
+                        driver.Navigate().GoToUrl(goToUrl);
+
+                    //driver.Manage().Window.Size = new System.Drawing.Size(1659, 917);
+                    driver.Manage().Window.Maximize();
+                    driver.FindElement(By.CssSelector("#checkout > .icon")).Click();
+
+                    log.Info("Checkout click Successful!!");
+                    Thread.Sleep(5000);
+
+
+                    try
+                    {
+                        IWebElement slotButton = driver.FindElement(By.CssSelector(".slotmodal-btn-confirm"));
+                        Thread.Sleep(5000);
+                        IWebElement slotcontent = driver.FindElement(By.XPath("//p[@id=\'slotContent\']"));
+                        Thread.Sleep(5000);
+
+                        vars["ConfirmationMsg"] = slotcontent.Text;
+                        string txt = vars["ConfirmationMsg"].ToString();
+
+
+                        if (txt.Length != 0)
+                        {
+                            log.Info(txt);
+                            Actions act = new Actions(driver);
+                            act.MoveToElement(slotButton).Click().Perform();
+
+                            if (txt.ToLower().Contains("unfortunately"))
+                            {
+                                log.Info("No slots available");
+                            }
+                            else
+                            {
+                                StopCheckout(settings);
+                                log.Info("Checkout Stopped");
+                                SendSMS("Might have been successful, please check", settings);
+                                
+                            }
+
+                        }
+                        else
+                        {
+                            log.Info(txt);
+                            StopCheckout(settings);
+                            log.Info("Checkout Stopped");
+                            SendSMS("Might have been successful, please check", settings);
+                            
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        StopCheckout(settings);
+                        log.Info("Checkout Stopped");
+                        SendSMS("Might have been successful, please check", settings);
+                        log.Error(ex.Message);
+                    }
+
+
+
+
+
+                    log.Info("Checkout ends!!");
+
+                    driver.Close();
+                    driver.Quit();
+
+                }
+                catch (Exception ex)
+                {
+                    StopCheckout(settings);
+                    log.Info("Checkout Stopped");
+                    SendSMS("has an error", settings);
+                    log.Error(ex.Message);
+                    driver.Close();
+                    driver.Quit();
+                }
+            }
+            else
+                log.Info("No profile presents");
+        }
+
+        private static void StopCheckout(KeyValueConfigurationCollection settings)
+        {
+
+            string presentValue = settings["RunJob"].Value;
+            if (presentValue.ToLower() == "yes")
+            {
+                settings["RunJob"].Value = "no";
+            }
+            config.Save();
+        }
+
+        private static void SendSMS(string success, KeyValueConfigurationCollection settings)
+        {
+
+            try
+            {
+                //Get value from AppSettings
+                string accountSid = ReturnSettings(settings, MyAppSettings.TwAccountSid);
+                //Get value from AppSettings
+                string authToken = ReturnSettings(settings, MyAppSettings.TwAuthToken);
+                //Get value from AppSettings
+                string twilioMoNum = ReturnSettings(settings, MyAppSettings.TwilioMoNum);
+                //Get value from AppSettings
+                string toMoNum = ReturnSettings(settings, MyAppSettings.ToMoNum);
+
+
+                if (accountSid == string.Empty)
+                    throw new Exception("No SID");
+                if (authToken == string.Empty)
+                    throw new Exception("No Auth Token");
+                if (twilioMoNum == string.Empty)
+                    throw new Exception("No Twillo Number");
+                if (toMoNum == string.Empty)
+                    throw new Exception("No Mobile Number to send messages");
+
+                TwilioClient.Init(accountSid, authToken);
+
+                //Get cell nums from AppSettings
+                if (toMoNum.Contains(","))
+                {
+                    string[] tonums = toMoNum.Split(',');
+                    for (int i = 0; i < tonums.Length; i++)
+                    {
+                        try
+                        {
+                            SendSMS(success, tonums[i].Trim());
+                        }
+                        catch (Exception ex)
+                        {
+                            log.Info("Message to " + tonums[i] + " has been " + "fail" + ".");
+                            log.Error(ex.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        SendSMS(success, toMoNum.Trim());
+                    }
+                    catch (Exception ex)
+                    {
+                        log.Info("Message to " + toMoNum + " has been " + "fail" + ".");
+                        log.Error(ex.Message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Info("SMS sent fail");
+                log.Error(ex.Message);
+            }
+
+        }
+
+        private static void SendSMS(string success, string tomo)
+        {
+            var message = MessageResource.Create(
+                                            body: "BigBasker Shoppinf checkout attempt " + success,
+                                            from: new Twilio.Types.PhoneNumber(twilioMoNum),
+                                            to: new Twilio.Types.PhoneNumber(tomo)
+                                        );
+            log.Info("Message to " + tomo + " has been " + message.Status + ".");
+        }
+
+        public class MyAppSettings
+        {
+            private MyAppSettings(string value) { Value = value; }
+
+            public string Value { get; set; }
+
+            public static MyAppSettings RunJob { get { return new MyAppSettings("RunJob"); } }
+            public static MyAppSettings UserProfilePath { get { return new MyAppSettings("UserProfilePath"); } }
+            public static MyAppSettings GoToUrl { get { return new MyAppSettings("GoToUrl"); } }
+            public static MyAppSettings TwilioMoNum { get { return new MyAppSettings("TwilioMoNum"); } }
+            public static MyAppSettings ToMoNum { get { return new MyAppSettings("ToMoNum"); } }
+            public static MyAppSettings TwAccountSid { get { return new MyAppSettings("TwAccountSid"); } }
+            public static MyAppSettings TwAuthToken { get { return new MyAppSettings("TwAuthToken"); } }
+        }
+
+
+    }
+}
+
+        
+
+
+
+    
+
